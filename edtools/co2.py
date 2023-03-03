f8603f4838ef84165ba424a4ce4eba64239d7cc7
altamarmx <gbv@ier.unam.mx>
2023-03-03 10:43:00 -0600

    formato y-axis

--- edtools/co2.py
+++ edtools/co2.py
@@ -6,44 +6,7 @@ from dateutil.parser import parse
 # colores
 azulier     = "#1A3D6F"
 doradoier  = '#C65C25'
-#  @file     enerdata.mplstyle
-#  @author   Guillermo Barrios <gbv@ier.unam.mx>
-#
-#  This mpl style is intended to be used in for the dataviz project at IER-UNAM
 
-# figure.figsize 		: 12.00, 6.00
-#
-# axes.spines.bottom  : True
-# axes.spines.left    : False
-# axes.spines.right   : False
-# axes.spines.top     : False
-# axes.prop_cycle		: cycler('color', [ 'grey', 'grey', 'grey', 'grey', 'grey'])
-# axes.labelcolor		: grey
-# axes.labelsize		: 26   # fontsize of the axes title
-# axes.grid           : True   # display grid or not
-# axes.grid.which     : major
-# axes.grid.axis      : y
-#
-# grid.linestyle   :   :       # dotted
-#
-#
-# xtick.labelsize		: 22 # fontsize of the tick labels
-# ytick.labelsize		: 22 # fontsize of the tick labels
-# legend.fontsize		: 22
-#
-#
-# lines.linewidth		: 8
-# lines.markersize	: 14            # markersize, in points
-#
-# font.size		: 20
-# #font.family		: sans-serif
-# #font.serif		: Tahoma
-#
-# xtick.color		: grey
-# ytick.color		: grey
-# text.color		: grey
-
-# savefig.transparent : True    # setting that controls whether figures are saved with a
 def load_co2():
     f = 'https://raw.githubusercontent.com/EneRDataMx/edtools/main/data/co2_mlo_surface-insitu_1_ccgg_DailyData.txt'
     df = pd.read_csv(f,skiprows=150,delimiter=' ',parse_dates={'date':[1,2,3,4,5,6]})
@@ -84,7 +47,7 @@ def co2_when_born(nombre,anio,mes,dia):
 
 
         ax.plot(df.value,lw=1,c=doradoier)
-        ax.set_ylabel("ppm CO2")
+        ax.set_ylabel("CO2 [ppm]")
 
         ax.set_xlim(min_date, max_date)
     #     ax.axhline(timeline, xmin=0.05, xmax=0.95, c='deeppink', zorder=2)
